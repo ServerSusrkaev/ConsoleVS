ac3ea62fbddc22bbd5b9a88b77dadecf4fcfdf53
Server Susurkaev <susurkaev@yandex.ru>
2023-03-19 10:05:50 +0300

    First commit

---
+++ ConsoleVS/Program.cs
@@ -0,0 +1,23 @@
+﻿using ListMaster;
+
+namespace ConsoleVS
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            ArrayMaster am = new();
+            for (int i = 0; i < 10; i++)
+            {
+                am.Add(new Random().Next(1, 5));
+            }
+            
+            for (int i = 0;i < am.GetCount();i++)
+            {
+                Console.WriteLine(am.GetValue(i));
+            }
+        }
+
+        
+    }
+}
